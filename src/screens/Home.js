1e26a2f94821d2d523f6de631adfc31213830fe2
Onkar Kanse <OnkarKanse18@hmail.com>
2023-01-28 17:06:44 +0530

    Able to Display

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,26 +6,36 @@ import axios from 'axios'
 const Home = () => {
 
   const [data, setdata] = useState([])
-  let i ="6"
+
   useEffect( () => {
     axios.get('http://localhost:8000/home').then((res)=>{
       setdata(res.data)
-       console.log(res.data[i]);
-      console.log(res.data);
-      console.log(res);
+      //  console.log(res.data[i]);
+      // console.log(res.data);
+      // console.log(res);
     })
     //  console.log(data[0].i);
     
   }, []);
-   Object.entries(data).map(x=>console.log(x[0]))
+    
+  //  Object.entries(data).map(
+  //             x=>console.log(x)
+  //   )
   
 
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
-         <View style={{flex:1,justifyContent:"center"}}>
-            <Text>
-                    {data[6]}
-            </Text>
+         <View style={{flex:0,justifyContent:"center"}}>
+            {
+              Object.entries((data)).map((x)=>{
+                    return(
+                      <View style={{backgroundColor:'red',height:15}}  key={x[0]}>
+                        <Text>{x[0]}</Text>
+                      </View>
+
+                    )
+              })
+            }
          </View>
     </SafeAreaView>
   )
